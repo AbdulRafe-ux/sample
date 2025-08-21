d15fee93fed806c9fa97bb2ae121e3ae280ad4bd
Abdul Rafe Afzal <rafe555afzal@gmail.com>
2025-08-21 23:48:16 +0530

    first commit

---
+++ src/test/java/com/example/demo/ProjectApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ProjectApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+}
